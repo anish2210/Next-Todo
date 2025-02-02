eec38b8fc4693ec84dcaf64b2f08bbac73650d14
anish2210 <anishjaiswal1220@gmail.com>
2025-02-03 01:29:48 +0530

    implemented postgre and prisma with singleton behaviour

---
+++ app/lib/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+// @ts-expect-error wtf
+const prisma = globalThis.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
+export default prisma;
